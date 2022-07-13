98ad0742bb85af8ac4559408d7dd9c944fed64fe
bergamos <alisson.bergs@gmail.com>
2022-07-12 21:08:16 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./assets/components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Olá Mundo!</h1>
+    <NotificationButton />
+    </>
+    
   )
 }
 
